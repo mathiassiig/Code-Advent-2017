e2fe7bc8427f751be1e4205f93346a06a844d1be
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-12-07 20:37:12 +0200

    Solved day 1 challenge 1

---
+++ CalendarCSharp/CalendarCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dayOne = new D1C1();
+            Console.WriteLine(dayOne.Output());
+        }
+    }
+}
