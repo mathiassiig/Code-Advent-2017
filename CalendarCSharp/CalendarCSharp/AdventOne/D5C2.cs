9f990db265752bf49b47bf9ff77ab2f5d460e477
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-12-11 19:32:37 +0200

    Solved day 5 challenge 2

---
+++ CalendarCSharp/CalendarCSharp/AdventOne/D5C2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarCSharp.AdventOne
+{
+    public class D5C2
+    {
+        public int Output()
+        {
+            var allText = System.IO.File.ReadAllText(System.IO.Path.Combine(Environment.CurrentDirectory, "d5_input.txt"));
+            var allLines = allText.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            var allNumbers = allLines.Select(x => Convert.ToInt32(x)).ToList();
+            int index = 0;
+            int jumps = 0;
+            while (index >= 0 && index < allNumbers.Count)
+            {
+                var oldIndex = index;
+                var offset = allNumbers[index];
+                index += offset;
+                if (offset >= 3)
+                {
+                    allNumbers[oldIndex] = allNumbers[oldIndex] - 1;
+                }
+                else
+                {
+                    allNumbers[oldIndex] = allNumbers[oldIndex] + 1;
+                }
+                jumps++;
+            }
+            return jumps;
+        }
+    }
+}
