188bdd6d256b587d1726f4b85cab704fc04dcba2
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2017-12-15 21:42:23 +0200

    Solved day 9 challenge 2

---
+++ CalendarCSharp/CalendarCSharp/AdventTwo/D9C2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarCSharp.AdventTwo
+{
+    public class D9C2
+    {
+        //private Dictionary<int, int> groups = new Dictionary<int, int>();
+        private int depthLevel = 0;
+        private int totalScore = 0;
+        public int Output()
+        {
+            var allText = System.IO.File.ReadAllText(System.IO.Path.Combine(Environment.CurrentDirectory, "d9_input.txt"));
+            bool garbage = false;
+            bool ignoreNext = false;
+            foreach (var c in allText)
+            {
+                if (ignoreNext)
+                {
+                    ignoreNext = false;
+                }
+                else
+                {
+                    if (garbage)
+                    {
+                        switch (c)
+                        {
+                            case '!':
+                                ignoreNext = true;
+                                break;
+                            case '>':
+                                garbage = false;
+                                break;
+                            default:
+                                totalScore++;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        switch (c)
+                        {
+                            case '!':
+                                ignoreNext = true;
+                                break;
+                            case '<':
+                                garbage = true;
+                                break;
+                            case '{':
+                                depthLevel++;
+                                break;
+                            case '}':
+                                depthLevel--;
+                                break;
+                        }
+                    }
+                }
+            }
+            return totalScore;
+        }
+
+
+
+    }
+}
